96493dedf7a394dc716a54bc4326d19b8de9bec6
Henrique Oliveira <henrique.oliveira83@yahoo.com.br>
2012-12-14 12:27:16 -0200

    Added Twig extensions submodule and changed init.php to autoload it

--- init.php
+++ init.php
@@ -2,6 +2,8 @@
 
 // Load the Twig class autoloader
 require_once Kohana::find_file('vendor', 'Twig/lib/Twig/Autoloader');
+require_once Kohana::find_file('vendor', 'Twig-extensions/lib/Twig/Extensions/Autoloader');
 
 // Register the Twig class autoloader
 Twig_Autoloader::register();
+Twig_Extensions_Autoloader::register();
