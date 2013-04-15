431660b117145f19ab6453831e8fd5d158a12e46
Henrique Oliveira <henrique.oliveira83@yahoo.com.br>
2013-04-15 14:05:30 -0300

    Removed Vendor modules

--- init.php
+++ init.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load the Twig class autoloader
-require_once Kohana::find_file('vendor', 'Twig/lib/Twig/Autoloader');
-require_once Kohana::find_file('vendor', 'Twig-extensions/lib/Twig/Extensions/Autoloader');
+//require_once Kohana::find_file('vendor', 'Twig/lib/Twig/Autoloader');
+//require_once Kohana::find_file('vendor', 'Twig-extensions/lib/Twig/Extensions/Autoloader');
 
 // Register the Twig class autoloader
-Twig_Autoloader::register();
-Twig_Extensions_Autoloader::register();
+//Twig_Autoloader::register();
+//Twig_Extensions_Autoloader::register();
